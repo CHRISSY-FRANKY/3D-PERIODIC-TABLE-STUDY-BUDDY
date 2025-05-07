fa42ad674729aee48cab043e341c858cd056993a
Christopher Francisco <cjfg00gl3@gmail.com>
2025-05-06 22:51:14 -0400

    ENHANCE THREEController class by adding properties and methods for WebGLRenderer and Scene management, improving the overall structure and functionality.

--- src/utils/THREEController.ts
+++ src/utils/THREEController.ts
@@ -1,45 +1,69 @@
 import * as THREE from "three";
 
 class THREEController {
-  private static instance: THREEController | null = null;
-  private mainContainer: HTMLElement | undefined;
-  private mcWidth: number | undefined;
-  private mcHeight: number | undefined;
 
-  public static getInstance(mainContainer: HTMLElement): THREEController {
-    if (!this.instance) {
-      this.instance = new THREEController(mainContainer);
+    private static instance: THREEController | null = null;
+
+    private mainContainer: HTMLElement | undefined;
+    private mcWidth: number | undefined;
+    private mcHeight: number | undefined;
+
+    private mcRenderer: THREE.WebGLRenderer | undefined;
+
+    private mcScene: THREE.Scene | undefined;
+
+    public static getInstance(mainContainer: HTMLElement): THREEController {
+        if (!this.instance) {
+            this.instance = new THREEController(mainContainer);
+        }
+        return this.instance;
     }
-    return this.instance;
-  }
 
-  private constructor(mainContainer: HTMLElement) {
-    this.setMainContainer(mainContainer);
-    this.setMCWidth(mainContainer.offsetWidth);
-    this.setMCHeight(mainContainer.offsetHeight);
-  }
+    private constructor(mainContainer: HTMLElement) {
+        this.setMainContainer(mainContainer);
+        this.setMCWidth(mainContainer.offsetWidth);
+        this.setMCHeight(mainContainer.offsetHeight);
+        this.setMCRenderer(new THREE.WebGLRenderer());
+        this.setMCScene(new THREE.Scene());
+    }
 
-  private setMainContainer(mainContainer: HTMLElement): void {
-    this.mainContainer = mainContainer;
-  }
+    private setMainContainer(mainContainer: HTMLElement): void {
+        this.mainContainer = mainContainer;
+    }
 
-  private getMainContainer(): HTMLElement | undefined{
-    return this.mainContainer;
-  }
+    private getMainContainer(): HTMLElement | undefined {
+        return this.mainContainer;
+    }
 
-  private setMCWidth(width: number): void {
-    this.mcWidth = width;
-  }
+    private setMCWidth(width: number): void {
+        this.mcWidth = width;
+    }
 
-  private setMCHeight(height: number): void {
-    this.mcHeight = height;
-  }
+    private setMCHeight(height: number): void {
+        this.mcHeight = height;
+    }
 
-  private getMCWidth(): number | undefined {
-    return this.mcWidth;
-  }
+    private getMCWidth(): number | undefined {
+        return this.mcWidth;
+    }
 
-  private getMCHeight(): number | undefined {
-    return this.mcHeight;
-  }
+    private getMCHeight(): number | undefined {
+        return this.mcHeight;
+    }
+
+    private setMCRenderer(renderer: THREE.WebGLRenderer): void {
+        this.mcRenderer = renderer;
+    }
+
+    private getMCRenderer(): THREE.WebGLRenderer | undefined {
+        return this.mcRenderer;
+    }
+
+    private setMCScene(scene: THREE.Scene) {
+        this.mcScene = scene;
+    }
+
+    private getMCScene(): THREE.Scene | undefined {
+        return this.mcScene;
+    }
 }
