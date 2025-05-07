d6f64ee6c995cdb00b5e5afe4b15714e83b56231
Christopher Francisco <cjfg00gl3@gmail.com>
2025-05-06 22:24:53 -0400

    REFINE THREEController class by improving code formatting, ensuring consistent use of quotes, and updating getter methods to return optional types for main container dimensions.

--- src/utils/THREEController.ts
+++ src/utils/THREEController.ts
@@ -1,52 +1,45 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-class THREEController
-{
-    private static instance:THREEController | null = null;
-    private mainContainer: HTMLElement | undefined;
-    private mcWidth:number | undefined;
-    private mcHeight:number | undefined;
-    
+class THREEController {
+  private static instance: THREEController | null = null;
+  private mainContainer: HTMLElement | undefined;
+  private mcWidth: number | undefined;
+  private mcHeight: number | undefined;
 
-    public static getInstance(mainContainer: HTMLElement): THREEController
-    {
-        if (!this.instance)
-        {
-            this.instance = new THREEController(mainContainer);
-        }
-        return this.instance;
+  public static getInstance(mainContainer: HTMLElement): THREEController {
+    if (!this.instance) {
+      this.instance = new THREEController(mainContainer);
     }
+    return this.instance;
+  }
 
-    private constructor(mainContainer: HTMLElement)
-    {
-        this.setMainContainer(mainContainer);
-        this.setMCWidth(mainContainer.offsetWidth);
-        this.setMCHeight(mainContainer.offsetHeight);
-    }
+  private constructor(mainContainer: HTMLElement) {
+    this.setMainContainer(mainContainer);
+    this.setMCWidth(mainContainer.offsetWidth);
+    this.setMCHeight(mainContainer.offsetHeight);
+  }
 
-    private setMainContainer(mainContainer: HTMLElement):void
-    {
-        this.mainContainer = mainContainer;
-    }
+  private setMainContainer(mainContainer: HTMLElement): void {
+    this.mainContainer = mainContainer;
+  }
 
-    private getMainContainer(): HTMLElement
-    {
-        return this.mainContainer;
-    }
+  private getMainContainer(): HTMLElement {
+    return this.mainContainer;
+  }
 
-    private setMCWidth(width: number):void {
-        this.mcWidth = width;
-    }
+  private setMCWidth(width: number): void {
+    this.mcWidth = width;
+  }
 
-    private setMCHeight(height: number):void {
-        this.mcHeight = height;
-    }
+  private setMCHeight(height: number): void {
+    this.mcHeight = height;
+  }
 
-    private getMCWidth(): number {
-        return this.mcWidth;
-    }
+  private getMCWidth(): number | undefined {
+    return this.mcWidth;
+  }
 
-    private getMCHeight(): number {
-        return this.mcHeight;
-    }
+  private getMCHeight(): number | undefined {
+    return this.mcHeight;
+  }
 }
