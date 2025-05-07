2643d331784a0fa132daadbab99d95a570d93615
Christopher Francisco <cjfg00gl3@gmail.com>
2025-05-06 22:21:07 -0400

    REFINE THREEController class by updating property types to be optional and introducing dedicated methods for setting and getting main container dimensions.

--- src/utils/THREEController.ts
+++ src/utils/THREEController.ts
@@ -3,9 +3,9 @@ import * as THREE from 'three';
 class THREEController
 {
     private static instance:THREEController | null = null;
-    private mainContainer: HTMLElement;
-    private mcWidth:number;
-    private mcHeight:number;
+    private mainContainer: HTMLElement | undefined;
+    private mcWidth:number | undefined;
+    private mcHeight:number | undefined;
     
 
     public static getInstance(mainContainer: HTMLElement): THREEController
@@ -19,13 +19,34 @@ class THREEController
 
     private constructor(mainContainer: HTMLElement)
     {
+        this.setMainContainer(mainContainer);
+        this.setMCWidth(mainContainer.offsetWidth);
+        this.setMCHeight(mainContainer.offsetHeight);
+    }
+
+    private setMainContainer(mainContainer: HTMLElement):void
+    {
         this.mainContainer = mainContainer;
-        this.mcWidth = mainContainer.offsetWidth;
-        this.mcHeight = mainContainer.offsetHeight;
     }
 
     private getMainContainer(): HTMLElement
     {
         return this.mainContainer;
+    }
+
+    private setMCWidth(width: number):void {
+        this.mcWidth = width;
+    }
+
+    private setMCHeight(height: number):void {
+        this.mcHeight = height;
+    }
+
+    private getMCWidth(): number {
+        return this.mcWidth;
+    }
+
+    private getMCHeight(): number {
+        return this.mcHeight;
     }
 }
